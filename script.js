e2024e45a9eef6a31829e22843fd7a938375592b
gusboll <augusto.sboll@gmail.com>
2025-04-28 10:46:57 -0300

    finish the project
    added the darken hover option and the colored hover option on js
    added the two compatible buttons on html

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const container = document.querySelector(".container");
 let divs = [];
-//for (let i = 0; i<256; i++){
-    //const div = document.createElement("div")
-    //div.style.width = "25px";
-    //div.style.height = "25px";
-    //container.appendChild(div);
-    //divs.push(div);
-//}
 
-
-
-//popup asking the number of squares per side of the new grid
-
+//button 0
 const button = document.querySelector(".btn");
 button.addEventListener("click", function(){
     const userWidth = prompt("Set your width");
     const userHeight = prompt("Set your height");
     let gridNumber = userWidth * userHeight;
 
+//checks if the user input was in the limits below
     if (gridNumber === 0){
         alert("Invalid numbers, set your grid again")
         return;
+    } else if (userHeight >=100){
+        alert("Impossible to generate a grid bigger than 99x99, please set the measures again");
+        gridNumber = 0
+    } else if (userWidth >= 100){
+        alert("Impossible to generate a grid bigger than 99x99, please set the measures again");
+        gridNumber = 0
     } else if (userWidth !== userHeight){
         alert("You grid may be distorted. If you do not want it to be distorted, you must set the same width and lenght");
     }
 
+//after the button and the checking, sets and executes the function
     function setGrid(){
         container.innerHTML = "";
         divs = [];
@@ -46,5 +44,98 @@ button.addEventListener("click", function(){
     setGrid();
 })
 
+//button 1
+const button1 = document.querySelector(".btn1");
+button1.addEventListener("click", function(){
+    const userWidth1 = prompt("Set your width");
+    const userHeight1 = prompt("Set your height");
+    let gridNumber1 = userWidth1 * userHeight1;
+
+//checks if the user input was in the limits below
+    if (gridNumber1 === 0){
+        alert("Invalid numbers, set your grid again")
+        return;
+    } else if (userHeight1 >=100){
+        alert("Impossible to generate a grid bigger than 99x99, please set the measures again");
+        gridNumber = 0
+    } else if (userWidth1 >= 100){
+        alert("Impossible to generate a grid bigger than 99x99, please set the measures again");
+        gridNumber = 0
+    } else if (userWidth1 !== userHeight1){
+        alert("You grid may be distorted. If you do not want it to be distorted, you must set the same width and lenght");
+    }
+ 
+//after the button and the checking, sets and executes the function of colored grid
+    function setGridColored(){
+        container.innerHTML = "";
+        divs = [];
+        const baseWidthPercent1 = 100 / userWidth1;
+
+        for (let i = 0; i<gridNumber1; i++){
+            const div = document.createElement("div");
+            div.style.flexBasis = `${baseWidthPercent1}%`;
+            div.style.aspectRatio = "1 / 1";
+            container.appendChild(div);
+            divs.push(div);
+        }
+        divs.forEach(function(div){
+            div.addEventListener("mouseover", function(){
+                let symbols = "0123456789ABCDEF";
+                let color = "#";
+                for (let i = 0; i<6;i++){
+                    color += symbols[Math.floor(Math.random() * 16)];
+                }
+                div.style.backgroundColor = color;
+                //div.style.backgroundColor = "black";
+            })
+        })
+    }
+    setGridColored();
+})
 
 
+//button 2
+const button2 = document.querySelector(".btn2");
+button2.addEventListener("click", function(){
+    const userWidth2 = prompt("Set your width");
+    const userHeight2 = prompt("Set your height");
+    let gridNumber2 = userWidth2 * userHeight2;
+
+//checks if the user input was in the limits below
+    if (gridNumber2 === 0){
+        alert("Invalid numbers, set your grid again")
+        return;
+    } else if (userHeight2 >=100){
+        alert("Impossible to generate a grid bigger than 99x99, please set the measures again");
+        gridNumber = 0
+    } else if (userWidth2 >= 100){
+        alert("Impossible to generate a grid bigger than 99x99, please set the measures again");
+        gridNumber = 0
+    } else if (userWidth2 !== userHeight2){
+        alert("You grid may be distorted. If you do not want it to be distorted, you must set the same width and lenght");
+    }
+ 
+//after the button and the checking, sets and executes the function of colored grid
+    function setGridDarken(){
+        container.innerHTML = "";
+        divs = [];
+        const baseWidthPercent2 = 100 / userWidth2;
+
+        for (let i = 0; i<gridNumber2; i++){
+            const div = document.createElement("div");
+            div.style.flexBasis = `${baseWidthPercent2}%`;
+            div.style.aspectRatio = "1 / 1";
+            container.appendChild(div);
+            divs.push(div);
+        }
+        divs.forEach(function(div){
+            div.addEventListener("mouseover", function(){
+                if (div.style.opacity <= 0.9){
+                    div.style.opacity = +div.style.opacity + 0.1;
+                }
+                div.style.backgroundColor = "black";
+            })
+        })
+    }
+    setGridDarken();
+})
